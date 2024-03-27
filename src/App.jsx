5a492e6f3e6fd92d2b893c5f98b1162077b8f36d
Rich-1007 <ghricheak@gmail.com>
2024-03-27 22:17:24 +0530

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Grid from "./Component/Grid";
+
 import MainItem from "./Component/MainItem";
-import Testing from "./Component/Testing";
 
 function App() {
   return (
     <>
-      {/* <Grid /> */}
+    
       <MainItem />
-      {/* <Testing /> */}
+  
     </>
   );
 }
